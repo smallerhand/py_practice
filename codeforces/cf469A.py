da8eed12805233e8c43ec92d73bad5bcf92f0158
smallerhand <mrmu0124@gmail.com>
2018-07-24 00:32:16 +0900

    all

--- codeforces/cf469A.py
+++ codeforces/cf469A.py
@@ -4,4 +4,9 @@ aline = input().split(' ')
 p = [int(i) for i in pline]
 a = [int(i) for i in aline]
 
+if len(set(p[1:]+a[1:]))==n:
+    print('I become the guy.')
+else:
+    print('Oh, my keyboard!')
+
 
