90f42a166e256c8dcff78cd9e50a24a9c9337a86
smallerhand <mrmu0124@gmail.com>
2018-07-25 00:21:48 +0900

    all

--- codeforces/cf59A.py
+++ codeforces/cf59A.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 25 00:10:53 2018
+s = input()
 
-@author: kimseunghyuck
-"""
+output1 = ''
+output2 = ''
+small = 0
+capital = 0
+for i in s:
+    if ord(i)>=65 and ord(i)<=90:
+        output1 += chr(ord(i)+32)
+        output2 += i
+        capital += 1
+    else:
+        output1 += i
+        output2 += chr(ord(i)-32)        
+        small += 1
 
+if small >= capital:
+    print(output1)
+else:
+    print(output2)
