865d2a451c1bcfbeda61bb82ecf4dc4d2e8a8229
smallerhand <mrmu0124@gmail.com>
2018-07-28 00:04:28 +0900

    all

--- codeforces/cf734A.py
+++ codeforces/cf734A.py
@@ -1,2 +1,17 @@
 n=int(input())
 s=input()
+
+A=0
+D=0
+
+for i in s:
+    if i=='A':
+        A+=1
+    else:
+        D+=1
+if A==D:
+    print('Friendship')
+elif A>D:
+    print('Anton')
+else:
+    print('Danik')
